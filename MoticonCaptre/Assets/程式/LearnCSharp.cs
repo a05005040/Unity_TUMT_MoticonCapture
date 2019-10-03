7117d3bc457f9b73a9d42052d524ac8c71b3e046
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-10-03 10:41:36 +0800

    靜態與非靜態類別

---
+++ MoticonCaptre/Assets/程式/LearnCSharp.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+public class LearnCSharp : MonoBehaviour
+{
+    [Header("分數")]
+    [Range(0,100)]
+    public int score = 60;                       //整數
+    [Header("速度"),Tooltip("可以放紅色與藍色藥水")]
+    public float speed = 1.5f;                   //浮點數
+    [Header("道具")]
+    public string prop = "藥水";                 //字串
+    [Header("任務")]
+    public bool mission = false;                 //布林值
+
+    public Vector2 v2 = new Vector2(7, 77);
+    public Vector3 v3 = new Vector3(1, 2, 3);
+
+    public Color blue = Color.blue;
+    public Color red = new Color(0.7f,0.1f,0.2f);
+
+    public AudioClip sound;
+    public Camera cam;
+    public Light lig;
+    public Transform camPos;
+    public GameObject obj;
+
+    public Debug deb;
+
+
+    private void Start()
+    {
+        //非靜態類別
+        //Camera.depth = 10.5f;//
+        cam.depth = 10.5f;
+
+        //靜態類別
+        Debug.Log("123");
+    }
+}

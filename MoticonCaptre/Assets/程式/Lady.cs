9e34b0072a787b0ce355f9871b57278523712ef9
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-10-31 10:25:54 +0800

    角色控制類別方法

---
+++ MoticonCaptre/Assets/程式/Lady.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class Lady : MonoBehaviour
+{
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    void Walk()
+    {
+
+    }
+    /// <summary>
+    /// 攻擊
+    /// </summary>
+    void Attack()
+    {
+
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    void Jump()
+    {
+
+    }
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    void Hurt()
+    {
+
+    }
+    /// <summary>
+    /// 死亡
+    /// </summary>
+    void Dead()
+    {
+
+    }
+
+}

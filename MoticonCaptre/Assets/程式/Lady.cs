32f7fc06a41c349a79565aef2a6453e4e41a6e79
a05005040 <55218579+a05005040@users.noreply.github.com>
2019-11-21 10:30:19 +0800

    添加旋轉欄位並使用一幀的時間解決不同電腦速度差問題

--- MoticonCaptre/Assets/程式/Lady.cs
+++ MoticonCaptre/Assets/程式/Lady.cs
@@ -14,6 +14,8 @@ public class Lady : MonoBehaviour
 
     [Header("速度"), Range(0f, 80f)]
     public float speed = 1.5f;
+    [Header("旋轉速度"), Range(1f, 100f)]
+    public float turn = 1.5f;
     private void Start()
     {
         ani = GetComponent<Animator>();
@@ -21,8 +23,13 @@ public class Lady : MonoBehaviour
     }
     private void Update()
     {
-        Walk();
+        Turn();
         Attack();
+       
+    }
+    private void FixedUpdate()
+    {
+        Walk();
         Jump();
     }
 
@@ -32,8 +39,13 @@ public class Lady : MonoBehaviour
     void Walk()
     {
         ani.SetBool(parRun, Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0);
-        //rig.AddForce(0, 0, Input.GetAxisRaw("Vertical") * speed);             // 以世界座標移動
-        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed); // 以區域座標移動
+        //rig.AddForce(0, 0, Input.GetAxisRaw("Vertical") * speed);                 // 以世界座標移動
+        //rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed);   // 以區域座標移動
+
+        // 前方 transform.forward (0, 0, 1)
+        // 右方 transform.right   (1, 0, 0)
+        // 上方 transform.up      (0, 1, 0)
+        rig.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * speed + transform.right * Input.GetAxisRaw("Horizontal") * speed);
     }
     /// <summary>
     /// 攻擊
@@ -69,6 +81,16 @@ public class Lady : MonoBehaviour
     {
         ani.SetBool(parDead, true);
 
+    }
+    /// <summary>
+    /// 左右旋轉
+    /// </summary>
+    private void Turn()
+    {
+        float x = Input.GetAxis("Mouse X");   // 滑鼠左右，左 -1、右 1
+        //print("玩家滑鼠 X：" + x);
+        // Time.deltaTime 一幀的時間
+        transform.Rotate(0, x * turn * Time.deltaTime, 0);
     }
 
 }
